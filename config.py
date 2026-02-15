0100e5e1a906a9d2f5854fa00408c07dba1aa5e0
ashar-codes <muhammadashar0300@gmail.com>
2026-02-15 22:19:13 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,10 @@
-iimport streamlit as st
+import streamlit as st
 
-# =============================
-# API KEY ACCESS
-# =============================
 def get_groq_api_key():
     try:
         return st.secrets["groq"]["api_key"]
     except Exception:
         return None
-
-
-# =============================
-# MODEL CONFIGURATION
-# =============================
 
 ANALYSIS_MODEL = "llama-3.1-8b-instant"
 RESPONSE_MODEL = "llama-3.1-8b-instant"
